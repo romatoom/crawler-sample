0dac59b16b55785bd35d515e5e2cc2fec29b8d37
Roman Ivanov <rom.i@citbits.com>
2023-05-29 17:13:44 +0500

    Возможность обновления базы данных с сохранением порядка записей (без пересоздания БД с нуля)

--- src/utils/exporter.js
+++ src/utils/exporter.js
@@ -1,12 +1,12 @@
 import fs from "fs";
-import { copyFile } from "node:fs/promises";
 import sqlite3 from "sqlite3";
+import { copyFile } from "node:fs/promises";
 import { open } from "sqlite";
 import { log } from "crawlee";
+import { snakeCase } from "snake-case";
+import { MI_FORMATTERS } from "#utils/formatters.js";
 
-import {
-  pathOfPreparedEntity
-} from "#utils/paths.js";
+import { pathOfPreparedEntity } from "#utils/paths.js";
 
 log.setLevel(log.LEVELS.INFO);
 
@@ -15,11 +15,15 @@ const sqlite = sqlite3.verbose();
 let sql;
 let db;
 
-async function recreateSqliteDBFile(sourceName) {
+async function prepareSqliteDBFile(sourceName) {
   try {
-    await copyFile("databases/empty.db", `databases/${sourceName}.db`);
+    const dbFilenamePath = `databases/${sourceName}.db`;
+
+    if (!fs.existsSync(dbFilenamePath)) {
+      await copyFile("databases/empty.db", dbFilenamePath);
+    }
   } catch (err) {
-    log.error(`Error while recreating database file "${sourceName}.db":`, err);
+    log.error(`Error while preparing database file "${sourceName}.db":`, err);
   }
 }
 
@@ -30,20 +34,36 @@ function readJSONData(sourceName) {
   rawData = fs.readFileSync(pathOfPreparedEntity(sourceName, "manuals"));
   const manuals = JSON.parse(rawData);
 
-  rawData = fs.readFileSync(pathOfPreparedEntity(sourceName, "products_manuals"));
-  const productsManuals = JSON.parse(rawData);
-
   return {
     products,
     manuals,
-    productsManuals,
   };
+}
+
+function productsManualsReferences(products, manuals) {
+  const references = [];
+
+  for (const [productIndex, product] of products.entries()) {
+    for (const [manualIndex, manual] of manuals.entries()) {
+      if (
+        product.name.toLowerCase() ===
+        MI_FORMATTERS.cleanedManualTitle(manual.title).toLowerCase()
+      ) {
+        references.push({
+          product_id: productIndex + 1,
+          manual_id: manualIndex + 1,
+        });
+      }
+    }
+  }
+
+  return references;
 }
 
 export default async function exportDataToSqlite(sourceName) {
   log.info(`Start exporting data to SQLite ("${sourceName}.db").`);
 
-  await recreateSqliteDBFile(sourceName);
+  await prepareSqliteDBFile(sourceName);
 
   const { products, manuals, productsManuals } = readJSONData(sourceName);
 
@@ -56,53 +76,187 @@ export default async function exportDataToSqlite(sourceName) {
     return log.error(err.message);
   }
 
+  const exportStatistic = {
+    products: {
+      insertedCount: 0,
+      updatedCount: 0,
+    },
+
+    manuals: {
+      insertedCount: 0,
+      updatedCount: 0,
+    },
+
+    productsManuals: {
+      insertedCount: 0,
+    },
+  };
+
   try {
-    // Insert products to DB
+    // Insert or update products to DB
+
     for (const product of products) {
-      sql = `INSERT INTO products(brand, category, name, url, specs, images) VALUES (?, ?, ?, ?, json(?), json(?))`;
+      sql = "SELECT id FROM products WHERE name = ?";
 
-      const values = [
-        product.brand,
-        product.category,
-        product.name,
-        product.url,
-        JSON.stringify(product.specs),
-        JSON.stringify(product.images),
-      ];
-
-      await db.run(sql, values, (err) => {
-        if (err) log.error(err.message);
+      // Find product
+      const productId = await db.get(sql, [product.name], (err, row) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        return row?.id;
       });
+
+      // Update row, if product exist in DB
+      if (productId) {
+        sql =
+          "UPDATE products SET brand = ?, category = ?, url = ?, specs = json(?), images = json(?) WHERE id = ?";
+
+        const values = [
+          product.brand,
+          product.category,
+          product.url,
+          JSON.stringify(product.specs),
+          JSON.stringify(product.images),
+          productId,
+        ];
+
+        await db.run(sql, values, function (err) {
+          if (err) return log.error(err.message);
+        });
+
+        exportStatistic.products.updatedCount++;
+      } else {
+        // Insert row, if product not exist in DB
+        sql = `INSERT INTO products(brand, category, name, url, specs, images) VALUES (?, ?, ?, ?, json(?), json(?))`;
+
+        const values = [
+          product.brand,
+          product.category,
+          product.name,
+          product.url,
+          JSON.stringify(product.specs),
+          JSON.stringify(product.images),
+        ];
+
+        await db.run(sql, values, (err) => {
+          if (err) return log.error(err.message);
+        });
+
+        exportStatistic.products.insertedCount++;
+      }
     }
 
-    // Insert manuals to DB
+    // Insert or update manuals to DB
     for (const manual of manuals) {
-      sql = `INSERT INTO manuals(material_type, pdf_url, title, languages) VALUES (?, ?, ?, json(?))`;
+      sql = "SELECT id FROM manuals WHERE pdf_url = ?";
 
-      const values = [
-        manual.materialType,
-        manual.pdfUrl,
-        manual.title,
-        JSON.stringify(manual.languages),
-      ];
-
-      await db.run(sql, values, (err) => {
-        if (err) log.error(err.message);
+      // Find manual
+      const manualId = await db.get(sql, [manual.pdfUrl], (err, row) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        return row?.id;
       });
+
+      // Update row, if manual exist in DB
+      if (manualId) {
+        sql =
+          "UPDATE manuals SET material_type = ?, title = ?, languages = json(?) WHERE id = ?";
+
+        const values = [
+          manual.materialType,
+          manual.title,
+          JSON.stringify(manual.languages),
+          manualId,
+        ];
+
+        await db.run(sql, values, function (err) {
+          if (err) return log.error(err.message);
+        });
+
+        exportStatistic.manuals.updatedCount++;
+      } else {
+        // Insert row, if maniual not exist in DB
+        sql = `INSERT INTO manuals(material_type, pdf_url, title, languages) VALUES (?, ?, ?, json(?))`;
+
+        const values = [
+          manual.materialType,
+          manual.pdfUrl,
+          manual.title,
+          JSON.stringify(manual.languages),
+        ];
+
+        await db.run(sql, values, (err) => {
+          if (err) return log.error(err.message);
+        });
+
+        exportStatistic.manuals.insertedCount++;
+      }
     }
+
+    const dbProducts = await db.all("SELECT * FROM products", (err, rows) => {
+      if (err) {
+        return log.error(err);
+      }
+      return rows;
+    });
+
+    const dbManuals = await db.all("SELECT * FROM manuals", (err, rows) => {
+      if (err) {
+        return log.error(err);
+      }
+      return rows;
+    });
+
+    // get productsManuals references;
+    const productsManuals = productsManualsReferences(dbProducts, dbManuals);
 
     // Insert products_manuals to DB
     for (const productManual of productsManuals) {
-      sql = `INSERT INTO products_manuals(product_id, manual_id) VALUES (?, ?)`;
+      sql =
+        "SELECT id FROM products_manuals WHERE product_id = ? AND manual_id = ?";
 
-      const values = [productManual.product_id, productManual.manual_id];
+      // Find manual
+      const productManualId = await db.get(
+        sql,
+        [productManual.product_id, productManual.manual_id],
+        (err, row) => {
+          if (err) {
+            return console.error(err.message);
+          }
+          return row?.id;
+        }
+      );
 
-      await db.run(sql, values, (err) => {
-        if (err) log.error(err.message);
-      });
+      if (!productManualId) {
+        sql = `INSERT INTO products_manuals(product_id, manual_id) VALUES (?, ?)`;
+
+        const values = [productManual.product_id, productManual.manual_id];
+
+        await db.run(sql, values, (err) => {
+          if (err) return log.error(err.message);
+        });
+
+        exportStatistic.productsManuals.insertedCount++;
+      }
     }
 
     log.info("Data exported successfully.");
+    ["products", "manuals", "productsManuals"].forEach((entity) => {
+      const insertedCount = exportStatistic[entity].insertedCount;
+      if (insertedCount > 0) {
+        log.info(
+          `Inserted ${insertedCount} records to table "${snakeCase(entity)}".`
+        );
+      }
+
+      const updatedCount = exportStatistic[entity].updatedCount || 0;
+      if (updatedCount > 0) {
+        log.info(
+          `Updated ${updatedCount} records to table "${snakeCase(entity)}".`
+        );
+      }
+    });
   } catch (err) {
     log.error(err.message);
   } finally {
--- src/utils/post-processer.js
+++ src/utils/post-processer.js
@@ -1,7 +1,6 @@
 import fs from "fs";
 import pkg from "core-js/actual/array/group-by.js";
 
-import { MI_FORMATTERS } from "#utils/formatters.js";
 const { groupBy } = pkg;
 
 import { log } from "crawlee";
@@ -9,48 +8,6 @@ import { log } from "crawlee";
 import { pathOfEntity, pathOfPreparedEntity } from "#utils/paths.js";
 
 log.setLevel(log.LEVELS.INFO);
-
-/* function usedIndexes(products, manuals) {
-  const usedProductsIndexes = new Set();
-  const usedManualsIndexes = new Set();
-
-  for (const [productIndex, product] of products.entries()) {
-    for (const [manualIndex, manual] of manuals.entries()) {
-      if (
-        product.name.toLowerCase() ===
-        MI_FORMATTERS.cleanedManualTitle(manual.title).toLowerCase()
-      ) {
-        usedProductsIndexes.add(productIndex);
-        usedManualsIndexes.add(manualIndex);
-      }
-    }
-  }
-
-  return {
-    usedProductsIndexes: [...usedProductsIndexes],
-    usedManualsIndexes: [...usedManualsIndexes],
-  };
-} */
-
-function productsManualsReferences(products, manuals) {
-  const references = [];
-
-  for (const [productIndex, product] of products.entries()) {
-    for (const [manualIndex, manual] of manuals.entries()) {
-      if (
-        product.name.toLowerCase() ===
-        MI_FORMATTERS.cleanedManualTitle(manual.title).toLowerCase()
-      ) {
-        references.push({
-          product_id: productIndex + 1,
-          manual_id: manualIndex + 1,
-        });
-      }
-    }
-  }
-
-  return references;
-}
 
 export default async function postProcessingData(sourceName) {
   log.info("Start post-processing.");
@@ -76,30 +33,9 @@ export default async function postProcessingData(sourceName) {
     preparedManuals.push(manual);
   }
 
-  /* const { usedProductsIndexes, usedManualsIndexes } = usedIndexes(
-    products,
-    preparedManuals
-  );
-
-  const fullPreparedProducts = products.filter((_, index) => {
-    return usedProductsIndexes.includes(index);
-  });
-
-  const fullPreparedManuals = preparedManuals.filter((_, index) => {
-    return usedManualsIndexes.includes(index);
-  }); */
-
-  const references = productsManualsReferences(products, preparedManuals);
-
   let data = JSON.stringify(preparedManuals);
   fs.writeFileSync(pathOfPreparedEntity(sourceName, "manuals"), data);
 
   data = JSON.stringify(products);
   fs.writeFileSync(pathOfPreparedEntity(sourceName, "products"), data);
-
-  data = JSON.stringify(references);
-  fs.writeFileSync(pathOfPreparedEntity(sourceName, "products_manuals"), data);
 }
-
-// 2986 manuals, 275 products, 1301 products_manuals
-// 3124 manuals, 275 products, 1336 products_manuals
